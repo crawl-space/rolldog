837d111a447799a55d457cdc57ab1986e928238f
James Bowes <jbowes@repl.ca>
2015-02-08 17:34:18 -0400

    Add datadog events

--- rolldog.py
+++ rolldog.py
@@ -12,7 +12,9 @@ log = logging.getLogger("rolldog")
 
 ROLLBAR_URL = os.environ.get("ROLLBAR_URL")
 ROLLDOG_TOKEN = os.environ.get("ROLLDOG_TOKEN")
+
 DATADOG_API_KEY = os.environ.get("DATADOG_API_KEY")
+DATADOG_ENV_PREFIX = os.environ.get("DATADOG_ENV_PREFIX", "")
 
 SLACK_HOOK_URL = os.environ.get("SLACK_HOOK_URL")
 
@@ -46,14 +48,54 @@ class ResponseLoggerMiddleware(object):
             resp.status[:3]))
 
 
+class Event(object):
+
+    def __init__(self, data):
+        event_name = data["event_name"]
+
+        self.is_deploy = False
+        if event_name == "new_item":
+            self.pretext = "New item"
+            self.level = "error"
+        elif event_name == "reactivated_item":
+            self.pretext = "Reactivated item"
+            self.level = "error"
+        elif event_name == "resolved_item":
+            self.pretext = "Resolved item"
+            self.level = "good"
+        elif event_name == "exp_repeat_item":
+            self.pretext = "Resolved item"
+            self.level = "warning"
+        elif event_name == "reopened_item":
+            self.pretext = "Reopened item"
+            self.level = "warning"
+        elif event_name == "deploy":
+            self.is_deploy = True
+            self.pretext = "Deploy"
+            self.level = "info"
+        else:
+            raise Exception("Unknown event type: %s" % event_name)
+
+        if self.is_deploy:
+            self.payload = data["data"]["deploy"]
+            self.title = self.payload["comment"]
+        else:
+            self.payload = data["data"]["item"]
+            self.title = "#{} {}".format(self.payload["counter"],
+                    self.payload["title"])
+
+        self.fallback = "{}: {}".format(self.pretext, self.title)
+
+
 class RollbarResource:
 
     def on_post(self, req, resp):
         item = json.load(req.stream)
 
         try:
-            # self.send_to_datadog(item)
-            self.send_to_slack(item)
+            event = Event(item)
+            self.send_to_datadog(event)
+            self.send_to_slack(event)
         except:
             log.exception("Failed to send!")
             raise falcon.HTTPBadGateway("Error sending data",
@@ -61,65 +103,55 @@ class RollbarResource:
 
         resp = falcon.HTTP_204
 
-    def send_to_datadog(self, item):
-        text = notification.description + "\n"
-        text += "\n".join(["{} - {}".format(app.name, app.description)
-            for app in notification.apps])
+    def send_to_datadog(self, event):
+        alert_map = {
+                "error": "error",
+                "warning": "warning",
+                "good": "success",
+                "info": "info",
+                }
 
-        payload = {
-            "title": notification.title,
+
+        text = ""
+        aggregation_key = None
+        if not event.is_deploy:
+            aggregation_key = "rollbar:" + str(event.payload["counter"])
+            if ROLLBAR_URL:
+                text = "{}{}".format(ROLLBAR_URL, event.payload["counter"])
+
+        tags = ["rollbar", "{}:{}".format(DATADOG_ENV_PREFIX,
+            event.payload["environment"])]
+
+        msg = {
+            "title": event.fallback,
             "text": text,
-            "tags": ["rollbar"],
-            "alert_type": "warning" if notification.errored else "info",
+            "tags": tags,
+            "aggregation_key": aggregation_key,
+            "alert_type": alert_map[event.level],
             }
 
-        r = requests.post(url + "?api_key=" + DATADOG_API_KEY,
-                data=json.dumps(payload))
+        datadog_url = "https://app.datadoghq.com/api/v1/events"
+        r = requests.post(datadog_url + "?api_key=" + DATADOG_API_KEY,
+                data=json.dumps(msg))
         r.raise_for_status()
 
-    def send_to_slack(self, item):
+    def send_to_slack(self, event):
         if not SLACK_HOOK_URL:
             return
 
-        default_color = "#3399FF"
-        event_name = item["event_name"]
-
-        if event_name == "new_item":
-            pretext = "New item"
-            color = "danger"
-        elif event_name == "reactivated_item":
-            pretext = "Reactivated item"
-            color = "danger"
-        elif event_name == "resolved_item":
-            pretext = "Resolved item"
-            color = "good"
-        elif event_name == "exp_repeat_item":
-            pretext = "Resolved item"
-            color = "warning"
-        elif event_name == "reopened_item":
-            pretext = "Reopened item"
-            color = default_color
-        elif event_name == "deploy":
-            pretext = "Deploy"
-            color = default_color
-        else:
-            raise Exception("Unknown event type: %s" % event_name)
+        color_map = {
+            "error": "danger",
+            "warning": "warning",
+            "good": "good",
+            "info": "#3399FF",
+            }
 
         title_link = None
+        if not event.is_deploy and ROLLBAR_URL:
+            title_link = ROLLBAR_URL + str(event.payload["counter"])
 
-        if event_name == "deploy":
-            payload = item["data"]["deploy"]
-            title = payload["comment"]
-        else:
-            payload = item["data"]["item"]
-            title = "#{} {}".format(payload["counter"], payload["title"])
-            if ROLLBAR_URL:
-                title_link = ROLLBAR_URL + str(payload["counter"])
-
-        fallback = "{}: {}".format(pretext, title)
-
+        payload = event.payload
         fields = []
-
         if "first_occurrence_timestamp" in payload:
             fields.append({
                 "title": "First Seen",
@@ -155,11 +187,11 @@ class RollbarResource:
 
         msg = {
             "attachments": [{
-                "pretext": pretext,
-                "fallback": fallback,
-                "title": title,
+                "pretext": event.pretext,
+                "fallback": event.fallback,
+                "title": event.title,
                 "title_link": title_link,
-                "color": color,
+                "color": color_map[event.level],
                 "fields": fields,
                 }]
             }
